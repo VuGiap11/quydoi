eb543ffe1a2d1e17925f884c8d8fc60183aa038f
vu giap <vudinhgiap11@gmail.com>
2023-07-06 10:45:02 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tigiachuyendoi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            int sotien;
+            long rate = 23000;
+            Console.WriteLine("mời người dùng nhập vào giá trị cần chuyển đổi tiền: ");
+            sotien = int.Parse(Console.ReadLine());
+
+            long vnd = sotien* rate;
+            Console.WriteLine("so tien quy doi la: "+vnd);
+        }
+    }
+}
